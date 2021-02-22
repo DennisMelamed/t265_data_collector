032bb13ae57106551067f20d89e133d6bde827b8
DennisMelamed <dennis.melamed1@gmail.com>
2021-02-22 17:30:04 -0500

    updates

--- collect_t265_data.py
+++ collect_t265_data.py
@@ -13,7 +13,7 @@ import time
 from dashing import *
 import serial
 import os
-
+from datetime import datetime
 # Declare RealSense pipeline, encapsulating the actual device and sensors
 pipe = rs.pipeline()
 
@@ -49,70 +49,74 @@ update_display()
 update_dx = -1
 start_time = time.time()
 
-## open a serial terminal here, should lead to a logging start on the OLA if its in deep sleep mode
-with serial.Serial('/dev/ttyUSB0', 115200, timeout=1) as ser:
-    ui.items[0].items[0].append("system started")
-    update_display()
-    ## pull out the init info from the OLA
-    for idx in range(16):
-        ui.items[0].items[1].append(str(ser.readline()))
-    update_display()
+### open a serial terminal here, should lead to a logging start on the OLA if its in deep sleep mode
 
-    with open(f"t265_data/{time.time()}.txt", 'w') as f:
-        f.write("timestamp,posX,posY,posZ,quatW,quatX,quatY,quatZ,tracking_confidence,mapping_confidence,zed_aX,zed_aY,zed_aZ,zed_gX,zed_gY,zed_gZ\n")
-        try:
-            while True:
-                # Wait for the next set of frames from the camera
-                frames = pipe.wait_for_frames()
+#with serial.Serial('/dev/ttyUSB0', 115200, timeout=1) as ser:
+ui.items[0].items[0].append("system started")
+update_display()
+#    ## pull out the init info from the OLA
+#    for idx in range(16):
+        #ui.items[0].items[1].append(str(ser.readline()))
+#    update_display()
+ui.items[0].items[1].append("not using OLA")
+update_display()
+curDT = datetime.now()
+file_str = curDT.strftime("%Y-%m-%d-%H:%M:%S")
+with open("/home/dennis/t265_data_collector/t265_data/{}.txt".format(file_str), 'w') as f:
+    f.write("timestamp,posX,posY,posZ,quatW,quatX,quatY,quatZ,tracking_confidence,mapping_confidence,zed_aX,zed_aY,zed_aZ,zed_gX,zed_gY,zed_gZ\n")
+    try:
+        while True:
+            # Wait for the next set of frames from the camera
+            frames = pipe.wait_for_frames()
 
-                # Fetch pose frame
-                pose = frames.get_pose_frame()
-                if pose:
-                    data = pose.get_pose_data()
+            # Fetch pose frame
+            pose = frames.get_pose_frame()
+            if pose:
+                data = pose.get_pose_data()
 
 
-                    gyro  = frames[0].as_motion_frame().get_motion_data()
-                    accel = frames[1].as_motion_frame().get_motion_data()
+                gyro  = frames[0].as_motion_frame().get_motion_data()
+                accel = frames[1].as_motion_frame().get_motion_data()
 
-                    f.write("{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}\n".format(pose.timestamp,
-                                                                                    data.translation.x,
-                                                                                    data.translation.y,
-                                                                                    data.translation.z,
-                                                                                    data.rotation.w,
-                                                                                    data.rotation.x,
-                                                                                    data.rotation.y,
-                                                                                    data.rotation.z,
-                                                                                    data.tracker_confidence,
-                                                                                    data.mapper_confidence,
-                                                                                    accel.x,
-                                                                                    accel.y,
-                                                                                    accel.z,
-                                                                                    gyro.x,
-                                                                                    gyro.y,
-                                                                                    gyro.z,
-                                                                                    ))
+                f.write("{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}\n".format(pose.timestamp,
+                                                                                data.translation.x,
+                                                                                data.translation.y,
+                                                                                data.translation.z,
+                                                                                data.rotation.w,
+                                                                                data.rotation.x,
+                                                                                data.rotation.y,
+                                                                                data.rotation.z,
+                                                                                data.tracker_confidence,
+                                                                                data.mapper_confidence,
+                                                                                accel.x,
+                                                                                accel.y,
+                                                                                accel.z,
+                                                                                gyro.x,
+                                                                                gyro.y,
+                                                                                gyro.z,
+                                                                                ))
 
-                    update_dx += 1
-                    if update_dx % 2000 == 0 and run_dashing_display:
-                        ui.items[1].items[0].value = (data.tracker_confidence+1)*25
-                        ui.items[1].items[1].value = (data.mapper_confidence+1)*25
-                        ui.items[0].items[0].append(str((time.time() - start_time)/60))
-                        ui.items[0].items[1].append(str(ser.readline()))
-                        update_display()
-                        update_dx = 0
+                update_dx += 1
+                if update_dx % 2000 == 0 and run_dashing_display:
+                    ui.items[1].items[0].value = (data.tracker_confidence+1)*25
+                    ui.items[1].items[1].value = (data.mapper_confidence+1)*25
+                    ui.items[0].items[0].append(str((time.time() - start_time)/60))
+                    #ui.items[0].items[1].append(str(ser.readline()))
+                    update_display()
+                    update_dx = 0
 
-        finally:
-            ui.items[0].items[0].append("shutting down")
-            update_display()
+    finally:
+        ui.items[0].items[0].append("shutting down")
+        update_display()
 
-            ## send close commands to OLA ( newline, q, then y)
-            ser.write(b'\n')
-            time.sleep(2)
-            ser.write(b'q')
-            time.sleep(2)
-            ser.write(b'y')
+#        ## send close commands to OLA ( newline, q, then y)
+#        ser.write(b'\n')
+#        time.sleep(2)
+#        ser.write(b'q')
+#        time.sleep(2)
+#        ser.write(b'y')
 
-            pipe.stop()
+        pipe.stop()
 
-            os.system("clear")
-            print("done")
+        os.system("clear")
+        print("done")
--- start_t265_collection.sh
+++ start_t265_collection.sh
@@ -2,4 +2,5 @@
 mkdir -p t265_data
 tmux kill-session -t "t265"
 tmux new-session -d -s "t265"
-tmux send-keys -t 't265' 'python collect_t265_data.py' C-m
+tmux send-keys -t 't265' 'python3 collect_t265_data.py' C-m
+tmux attach -t t265
